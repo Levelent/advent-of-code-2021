b57eda0bf04c72ff932e8abc5fdb64f136dbd023
Levelent <31184681+Levelent@users.noreply.github.com>
2021-12-21 15:16:39 +0000

    day 7 complete

---
+++ day07/a.rs
@@ -0,0 +1,38 @@
+use std::io::{self, BufRead};
+use std::fs::File;
+use std::path::Path;
+use std::cmp;
+
+fn main() -> () {
+    let positions = read_lines("input.txt");
+
+    let min = positions.iter().min().unwrap();
+    let max = positions.iter().max().unwrap();
+    let mut min_tot = -1;
+
+    for align in *min..=*max {
+        // Normalise around the alignment point and sum displacement
+        let tot = positions.iter()
+        .cloned()
+        .map(|x| x - align)
+        .fold(0, |acc, x| acc + x.abs());
+
+        // Always assign total on first pass, otherwise take the minimum
+        if align == *min { min_tot = tot; }
+        else { min_tot = cmp::min(min_tot, tot); }
+    }
+    println!("{}", min_tot);
+}
+
+fn read_lines<P>(filename: P) -> Vec<i32> where P: AsRef<Path> {
+    // Converts input line to a vector of integers
+    
+    let file = File::open(filename).unwrap();
+    let mut line = String::new();
+    io::BufReader::new(file).read_line(&mut line).unwrap();
+
+    return line
+    .split(",")
+    .map(|x| x.trim().parse::<i32>().unwrap())
+    .collect::<Vec<_>>();
+}
